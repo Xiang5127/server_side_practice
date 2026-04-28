8483388c076feb535c522d5bd996aa6b4c53671c
unknown <xiang01527@gmail.com>
2026-04-28 11:30:30 +0800

    aded .sql practical7

---
+++ practical7.sql
@@ -0,0 +1,70 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 28, 2026 at 05:30 AM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `practical7`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `error_logs`
+--
+
+CREATE TABLE `error_logs` (
+  `id` int(11) NOT NULL,
+  `error_code` varchar(50) NOT NULL,
+  `error_message` text NOT NULL,
+  `error_time` datetime NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `error_logs`
+--
+
+INSERT INTO `error_logs` (`id`, `error_code`, `error_message`, `error_time`) VALUES
+(1, 'E_DIV_ZERO', 'Attempted division by zero, Please check your calculations', '2026-04-27 01:38:03'),
+(2, 'E_DIV_ZERO', 'Attempted division by zero, Please check your calculations', '2026-04-27 01:39:02'),
+(3, 'E_DIV_ZERO', 'Attempted division by zero, Please check your calculations', '2026-04-27 01:40:30'),
+(4, 'E_UNDEFINED_VAR', 'Detected an undefined variable in the code.', '2026-04-27 01:40:30');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `error_logs`
+--
+ALTER TABLE `error_logs`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `error_logs`
+--
+ALTER TABLE `error_logs`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
